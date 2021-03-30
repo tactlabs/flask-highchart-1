da7cd89aa6063310c7353df3a61262b8c6845c82
Divya-1200 <divya07msd@example.com>
2021-03-30 16:13:31 +0530

    high chart code added

--- app.py
+++ app.py
@@ -10,8 +10,9 @@ import random
 import json
 
 app  = Flask(__name__)
-PORT = 3024
+PORT = 3000
 
+FILEPATH = "data.json"
     
 @app.route("/", methods=["GET","POST"])
 def startpy():
@@ -21,8 +22,17 @@ def startpy():
         "Greetings" : "Tactlabs welcomes you"
     }
 
-    return render_template("index.html", result = result)
+    return render_template("index.html")
 
+@app.route("/data", methods=["GET","POST"])
+def read_json():
+
+        with open(FILEPATH) as json_file:
+            data = json.load(json_file)
+
+        print(data)
+
+        return render_template("index.html", result = data)
 
 if __name__ == "__main__":
     app.run( debug = True,host="0.0.0.0",port = PORT)
